a17818f9fb5b1acd8b453031969a025f79cfbe36
jazz-code <ren.meta@gmail.com>
2019-06-18 17:18:22 -0400

    wheel event on img

--- js/index.js
+++ js/index.js
@@ -20,10 +20,33 @@ nav.addEventListener('click', event => {
 // IMG double-click event
 const IMG = document.querySelector('.content-section img');
 IMG.addEventListener('dblclick', event => {
+    event.target.style.transition = "transform .6s"
     event.target.style.transform = "scale(1.3)"
 })
 IMG.addEventListener('click', event => {
     event.target.style.transform = "none"
 })
 
-console.log(IMG)
+// wheel scroll event
+const bottomIMG = document.querySelector('.content-destination img')
+let scale = -1;
+bottomIMG.addEventListener('wheel', zoom => {
+    zoom.preventDefault();
+
+    scale += zoom.deltaY * -0.01;
+    //Restrict Scale
+    scale = Math.min(Math.max(.125, scale), 4);
+    //Applies scale transform
+    bottomIMG.style.transform = `scale(${scale})`
+    bottomIMG.onwheel = zoom;
+})
+
+bottomIMG.addEventListener('mouseleave', event => {
+    bottomIMG.style.transform = "none"
+})
+
+
+console.log(bottomIMG)
+
+
+
