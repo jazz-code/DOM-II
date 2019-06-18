4740cdd19ab89251b203c077dd4cdc515a7b019e
jazz-code <ren.meta@gmail.com>
2019-06-18 16:02:52 -0400

    mouseenter/mouseleave, click, scroll event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// Your code goes here
+// Header mouseenter / mouseleave IMG event
+const headerImg = document.querySelector('header img');
+
+headerImg.addEventListener('mouseenter', event => {
+    headerImg.style.border = '2px solid red'})
+headerImg.addEventListener('mouseleave', event => {
+    headerImg.style.border = 'none'
+})
+
+// Nav click event
+const nav = document.querySelector('header nav');
+
+nav.addEventListener('click', event => {
+    event.target.textContent;
+    
+    const title = document.querySelector('h1.logo-heading');
+    title.textContent = event.target.textContent
+})
+
+// Nav scroll event
+const navA = document.querySelector('header nav a')[0];
+IMG.addEventListener('dblclick', event => {
+    IMG.classList.toggle('large')
+})
